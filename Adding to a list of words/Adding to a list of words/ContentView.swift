c749b57bc41584147c14d59937385cb197374a1a
Elias Breitenbach <elias.b@me.com>
2023-05-23 22:01:28 +0200

    Challenge solved

--- Adding to a list of words/Adding to a list of words/ContentView.swift
+++ Adding to a list of words/Adding to a list of words/ContentView.swift
@@ -16,22 +16,30 @@ struct ContentView: View {
     @State private var errorTitle = ""
     @State private var errorMessage = ""
     @State private var showingError = false
+    @State private var score = 0
+    
     
     var body: some View {
         NavigationView {
             List {
                 Section {
                     TextField("Enter your Word", text: $newWord)
+                        .padding()
                         .autocapitalization(.none)
                         .textInputAutocapitalization(.never)
                 }
                 Section {
-                        ForEach(usedWords, id: \.self) { word in
-                            HStack {
+                    ForEach(usedWords, id: \.self) { word in
+                        HStack {
                             Image(systemName: "\(word.count).circle")
                             Text(word)
                         }
                     }
+                }
+                HStack {
+                    Spacer()
+                    Text("Score: \(score)")
+                    Spacer()
                 }
             }
             .navigationTitle(rootWord)
@@ -47,29 +55,47 @@ struct ContentView: View {
             }
         }
     }
+    
+    
     func addNewWord() {
         let answer = newWord.lowercased().trimmingCharacters(in: .whitespacesAndNewlines)
-        guard answer.count >= 3 && newWord == newWord else { return }
+        guard answer.count >= 3 else { return }
         
+        
+        
+        guard sameWord(word: answer) else {
+            wordError(title: "Same Word", message: "You know!")
+            score -= 1
+            return
+        }
         guard isOriginal(word: answer) else {
             wordError(title: "Word used allready", message: "Be more orignal!")
+            score -= 1
             return
         }
         guard isPossible(word: answer) else {
             wordError(title: "Word not possible", message: "You can not spell the word from '\(rootWord)'!")
+            score -= 1
             return
         }
         guard isReal(word: answer) else {
             wordError(title: "Word not recognized", message: "You can't just make them up, you know!")
+            score -= 1
             return
         }
         
+        score += answer.count
         
         withAnimation {
             usedWords.insert(answer, at: 0)
         }
         newWord = ""
     }
+    
+    func reset() {
+            score = 0
+    }
+    
     func startGame() {
         // 1. Find the URL for start.txt in our app bundle
         if let startWordsURL = Bundle.main.url(forResource: "start", withExtension: "txt") {
@@ -77,19 +103,31 @@ struct ContentView: View {
             if let startWords = try? String(contentsOf: startWordsURL) {
                 // 3. Split the string up into an array of strings, splitting on line breaks
                 let allWords = startWords.components(separatedBy: "\n")
-
+                
                 // 4. Pick one random word, or use "silkworm" as a sensible default
-                    rootWord = allWords.randomElement() ?? "silkworm"
-
+                rootWord = allWords.randomElement() ?? "silkworm"
+                
                 // If we are here everything has worked, so we can exit
+                
+                usedWords.removeAll()
+                newWord.removeAll()
+                reset()
                 return
             }
         }
-
+        
         // If were are *here* then there was a problem – trigger a crash and report the error
         fatalError("Could not load start.txt from bundle.")
     }
     
+    func sameWord(word: String) -> Bool {
+        if rootWord == newWord {
+            wordError(title: "Nope", message: "Same Word!")
+            return false
+        }
+        return true
+    }
+
     func isOriginal(word: String) -> Bool {
         !usedWords.contains(word)
     }
